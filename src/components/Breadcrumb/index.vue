92b0167916525e9cb15d68872c7ceaa4c1b705d0
niemingyan <1942350658@qq.com>
2021-12-14 18:09:43 +0800

    fix(Breadcrumb.vue): 面包屑动画优化

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -1,11 +1,17 @@
 <template>
-  <el-breadcrumb class='breadcrumb' separator='/'>
+  <el-breadcrumb class="breadcrumb" separator="/">
     <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-    <template v-for='(item,index) in list' :key='item.meta.title'>
+    <transition-group name="breadcrumb">
       <!--最后一个面包屑不需要跳转链接-->
-      <el-breadcrumb-item :to='item.path' v-if='index < list.length-1'>{{ item.meta.title }}</el-breadcrumb-item>
-      <el-breadcrumb-item class='no-redirect' v-else>{{ item.meta.title }}</el-breadcrumb-item>
-    </template>
+      <el-breadcrumb-item v-for="(item, index) in list" :key="item.meta.title">
+        <router-link :to="item.path" v-if="index < list.length - 1">
+          {{ item.meta.title }}
+        </router-link>
+        <span class="no-redirect" v-else>
+          {{ item.meta.title }}
+        </span>
+      </el-breadcrumb-item>
+    </transition-group>
   </el-breadcrumb>
 </template>
 
@@ -14,8 +20,9 @@ import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
+// 当前路由matched的列表
 const list = computed(() => {
-  return route.matched.filter(it => it.meta.title && it.meta.icon)
+  return route.matched.filter((it) => it.meta.title && it.meta.icon)
 })
 </script>
 
@@ -26,11 +33,9 @@ const list = computed(() => {
   line-height: 50px;
   margin-left: 8px;
 
-  ::v-deep(.no-redirect) {
-    .el-breadcrumb__inner {
-      color: #97a8be;
-      cursor: text;
-    }
+  .no-redirect {
+    color: #97a8be;
+    cursor: text;
   }
 }
 </style>
