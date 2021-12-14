9e4174c02439c22d35a03b2441ee5c76d38ba0a5
niemingyan <1942350658@qq.com>
2021-12-14 09:53:45 +0800

    feat: 生成动态 menu 菜单

---
+++ src/views/layout/sidebar/SidebarItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <template v-for='item in routes' :key='item.path'>
+    <!-- 子集 menu 菜单 -->
+    <el-submenu v-if='routes.children'>
+      <template #title>
+        <i class='el-icon-location'></i>
+        <span>{{ item.meta.title }}</span>
+      </template>
+      <template v-for='it in item.children' :key='it.path'>
+        <el-menu-item :index='it.path'>{{ it.meta.title }}</el-menu-item>
+      </template>
+    </el-submenu>
+    <!-- 具体菜单项 -->
+    <el-menu-item :index='item.path' v-else>
+      <i class='el-icon-setting'></i>
+      <template #title>{{ item.meta.title }}</template>
+    </el-menu-item>
+  </template>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  routes: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
+<style lang='scss' scoped></style>
