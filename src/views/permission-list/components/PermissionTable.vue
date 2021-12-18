ffd40794aec6c141ef547c88aef9bc9db2ea44fc
niemingyan <1942350658@qq.com>
2021-12-18 23:22:57 +0800

    feat: 权限列表

---
+++ src/views/permission-list/components/PermissionTable.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-table
+    :data='permissionList'
+    border
+    size='large'
+    row-key='id'
+    :tree-props='{children: "children"}'
+  >
+    <el-table-column label='权限名称' prop='permissionName' align='left' width='150'></el-table-column>
+    <el-table-column label='权限标识' prop='permissionMark' width='350'></el-table-column>
+    <el-table-column label='权限描述' prop='permissionDesc'></el-table-column>
+  </el-table>
+</template>
+
+<script setup>
+import { inject } from 'vue'
+// 权限列表
+const permissionList = inject('permissionList')
+</script>
+
+<style lang='scss' scoped></style>
--- src/views/permission-list/index.vue
+++ src/views/permission-list/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <div class="">权限列表</div>
+  <div class='permission-page'>
+    <permission-table />
+  </div>
 </template>
 
 <script setup>
-import {} from 'vue'
+import { provide, ref } from 'vue'
+import { getPermissionListApi } from '@/api/permission'
+import PermissionTable from '@/views/permission-list/components/PermissionTable'
+
+const permissionList = ref(null)
+provide('permissionList', permissionList)
+getPermissionList()
+
+async function getPermissionList () {
+  permissionList.value = await getPermissionListApi()
+}
+
 </script>
 
-<style lang="scss" scoped></style>
+<style lang='scss' scoped></style>
